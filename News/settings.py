763c2c3bcfa75408027918c6cc4241ac0663461d
hopgausi <jspyhack@gmail.com>
2020-08-07 06:03:10 +0200

    initial commit

--- News/settings.py
+++ News/settings.py
@@ -109,7 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = ''
 
 
 STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
