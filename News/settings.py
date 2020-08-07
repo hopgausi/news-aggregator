4b1c20ef686908aa716c6c2a5d2d604803f0d8e0
hopgausi <jspyhack@gmail.com>
2020-08-07 04:53:24 +0200

    initial commit

--- News/settings.py
+++ News/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_VALUE') 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hopgausi-news-collection.herokuapp.com']
 
 
 # Application definition
